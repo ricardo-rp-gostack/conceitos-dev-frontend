f1fab0265a3dad56f6a9be0b42193aaeeebab066
moro <ricardoromero4444@gmail.com>
2020-05-06 12:42:12 -0400

    importando css e imagens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import './App.css'
+import backgroundImage from './assets/background.jpg'
+
 import Header from "./components/Header";
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
   return (
     <>
       <Header title="Batata" />
+
+      <img width={100} src={backgroundImage} alt="background"/>
+
       <ul>
         {projects.map((project) => (
           <li key={project}>{project}</li>
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,36 @@
-const path = require('path')
+const path = require("path");
 /* directly passing './src/index.js' and './public', may not work in non-Unix 
 systems, due to how paths are handled. Thus, we use path.resolve to guarantee 
 our app will run in any OS*/
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, "src", "index.js"),
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
   },
-  devServer:{
-    contentBase: path.resolve(__dirname, 'public'),
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
   },
   module: {
     rules: [
-      {  // Use babel-loader for .js files outside node_modules
+      {
+        // Use babel-loader for .js files outside node_modules
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        }
-      }
-    ]
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: { loader: "file-loader" },
+      },
+    ],
   },
 };
